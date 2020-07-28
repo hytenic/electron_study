a6759ee0c99d635e80ed4d18b354a0dad3e3679e
sean_park <19980819@naver.com>
2020-07-28 21:45:40 +0900

    [ADD] print 'hello world' from greeting.json on react [ADD] 'activate', 'window-all-close' fucntion

--- public/Main.js
+++ public/Main.js
@@ -1,33 +1,39 @@
-
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
-function createWindow() {
-    /*
-    * 넓이 1920에 높이 1080의 FHD 풀스크린 앱을 실행시킵니다.
-    * */
-    const win = new BrowserWindow({
-        width:1920,
-        height:1080
-    });
+let mainWindow;
 
-    /*
-    * ELECTRON_START_URL을 직접 제공할경우 해당 URL을 로드합니다.
-    * 만일 URL을 따로 지정하지 않을경우 (프로덕션빌드) React 앱이
-    * 빌드되는 build 폴더의 index.html 파일을 로드합니다.
-    * */
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
-        protocol: 'file:',
-        slashes: true
-    });
-
-    /*
-    * startUrl에 배정되는 url을 맨 위에서 생성한 BrowserWindow에서 실행시킵니다.
-    * */
-    win.loadURL(startUrl);
-
+function onClosed() {
+  mainWindow = null;
 }
 
-app.on('ready', createWindow);
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080
+  });
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl);
+  mainWindow.on('closed', onClosed);
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (!mainWindow) { 
+    createWindow();
+  }
+});
+
+app.on('ready', createWindow)
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const greeting = require('./greeting.json');
+
   return (
     <div className="App">
-      <div>Hello Electron!</div>
+      <div>Hello Electron!{greeting.message}</div>
     </div>
   );
 }
