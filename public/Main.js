2c1a9b7d7e23689a291472999d859e04391621eb
sean_park <19980819@naver.com>
2020-07-29 15:47:16 +0900

    [ADD] electron shopping list basic menubar, addItem window

--- public/Main.js
+++ public/Main.js
@@ -1,17 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 
 let mainWindow;
+let addWindow;
 
 function onClosed() {
-  mainWindow = null;
+  // mainWindow = null;
+  app.quit();
 }
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1920,
-    height: 1080
+    width: 1200,
+    height: 800
   });
 
   const startUrl = process.env.ELECTRON_START_URL || url.format({
@@ -21,8 +23,41 @@ function createWindow() {
   });
 
   mainWindow.loadURL(startUrl);
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
   mainWindow.on('closed', onClosed);
 }
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    width: 300,
+    height: 200,
+    title: 'Add Shopping List Title'
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, '/../src/addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  addWindow.loadURL(startUrl);
+
+  app.on('close', () => {
+    addWindow = null;
+  });
+}
+
+ipcMain.on('item:add', (e, item) => {
+  console.log(item);
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -31,9 +66,54 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (!mainWindow) { 
+  if (!mainWindow) {
     createWindow();
   }
 });
 
 app.on('ready', createWindow)
+
+const mainMenuTemplate = [
+  {
+    label: 'Electron'
+  },
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import { ipcRenderer } from 'electron';
 
 function App() {
 
   const greeting = require('./greeting.json');
+  // let items: Array<string> = [];
+  
+
+  // ipcRenderer.on('item:add', (e, item: string) => {
+  //   console.log(item);  
+  // });
 
   return (
     <div className="App">
       <div>Hello Electron!{greeting.message}</div>
+      {/* <ul></ul> */}
     </div>
   );
 }
